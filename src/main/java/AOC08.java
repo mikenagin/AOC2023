d3cde46e581111fe9a22494857483949d5f7b5d5
Michelle Margaret <99129484+michellemargaret@users.noreply.github.com>
2023-12-08 15:29:42 -0330

    Advent of Code December 8, 2023 Works for puzzle input in under a minute

--- src/main/java/AOC08.java
+++ src/main/java/AOC08.java
@@ -9,6 +9,8 @@ public class AOC08 {
     private List<String> input;
     private List<Boolean> moves;
     private Map<String, List<String>> maps;
+    private List<Long> endCounts;
+    private List<String> startingPoints;
 
     public AOC08(List<String> input) {
         this.input = input;
@@ -40,35 +42,68 @@ public class AOC08 {
 
     public Long stepsTakenPart2() {
         Boolean reachedEnd = false;
-        Long movesTaken = 0L;
-        Integer moveIndex = 0;
+        endCounts = new ArrayList<>();
 
-        List<String> currentLocation = maps.keySet().stream().filter(x -> x.charAt(2) == 'A').collect(Collectors.toList());
+        startingPoints = maps.keySet().stream().filter(x -> x.charAt(2) == 'A').collect(Collectors.toList());
 
-        while(!reachedEnd) {
-            Boolean nextMove = moves.get(moveIndex);
-            movesTaken++;
-            if (movesTaken % 100000000 == 0) System.out.print(movesTaken + " ");
+        startingPoints.parallelStream().forEach(start -> {
+            Long movesTaken = 0L;
+            Integer moveIndex = 0;
+            Boolean finished = false;
+            String currentLocation = start;
 
-            currentLocation = currentLocation.stream().map(
-                    m -> nextMove ? maps.get(m).get(0) : maps.get(m).get(1)
-                    ).collect(Collectors.toList());
+            while(!finished) {
+                Boolean nextMove = moves.get(moveIndex);
+                movesTaken++;
+                if (movesTaken % 100000000 == 0) System.out.print(movesTaken + " ");
 
-            if (currentLocation.stream().filter(x -> x.charAt(2) == 'Z').count() == currentLocation.size()) reachedEnd = true;
+                currentLocation = nextMove ? maps.get(currentLocation).get(0) : maps.get(currentLocation).get(1);
 
-            moveIndex++;
-            if (moveIndex == moves.size()) moveIndex = 0;
+                if (currentLocation.charAt(2) == 'Z') {
+                    // This wouldn't work for everything but works for this input
+                    // Should save this movesTaken associated with this start, and continue
+                    // until it develops a pattern that would repeat
+                    final Long movesTakenCopy = movesTaken;
+                    endCounts.add(movesTakenCopy);
+                    finished = true;
+                }
 
+                moveIndex++;
+                if (moveIndex == moves.size()) moveIndex = 0;
+            }
+        });
+
+        // There is one value for each start point in this example
+        // Lowest common multiple for these six is the answer
+
+        return lowestCommonMultiple(endCounts);
+    }
+
+    private Long lowestCommonMultiple(List<Long> values) {
+        Long lcm = 0L;
+        Boolean found = false;
+        Long multiplyBy = 1L;
+
+        while (!found) {
+            lcm = multiplyBy*values.get(0);
+            final Long lcmFinal = lcm;
+
+            // So I know it's still chugging
+            if (multiplyBy % 100000000 == 0) System.out.print(multiplyBy + " ");
+
+            if (values.stream().allMatch(x -> (lcmFinal % x) == 0)) found = true;
+
+            multiplyBy++;
         }
 
-        return movesTaken;
+        return lcm;
     }
 
     private void readInput() {
         moves = new ArrayList<>();
 
         List.of(input.get(0).split("")).stream().forEach(m -> {
-                if (m.equals("L")) moves.add(true); else moves.add(false);
+            if (m.equals("L")) moves.add(true); else moves.add(false);
         });
 
         maps = new HashMap<>();
